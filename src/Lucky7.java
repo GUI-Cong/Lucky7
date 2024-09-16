cadaa6294063177465096e84e9357a28c1110a2c
GUI-Cong <cong.gui@student.hamk.fi>
2024-09-16 20:09:41 +0800

    step 1 finished

--- src/Lucky7.java
+++ src/Lucky7.java
@@ -7,6 +7,14 @@ public class Lucky7 {
         int randNum1 = random.nextInt(11);
         int randNum2 = random.nextInt(11);
         int randNum3 = random.nextInt(11);
-        System.out.printf("%d %d %d",randNum1,randNum2,randNum3);
+        System.out.printf("%d %d %d \n",randNum1,randNum2,randNum3);
+        int LuckyNum = 7;
+
+        if(randNum1 == LuckyNum || randNum2 == LuckyNum || randNum3 == LuckyNum){
+            System.out.println("You won");
+        }
+        else 
+            System.out.println("You lost");
+        
     }
 }
