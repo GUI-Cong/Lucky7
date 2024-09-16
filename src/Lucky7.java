230a57a4a71112768ed6a810c506fb1f21f7fe18
GUI-Cong <cong.gui@student.hamk.fi>
2024-09-16 22:01:55 +0800

    step 3 finished

--- src/Lucky7.java
+++ src/Lucky7.java
@@ -7,13 +7,16 @@ public class Lucky7 {
         
         //System.out.printf("%d %d %d \n",randNum1,randNum2,randNum3);
         int LuckyNum = 7;
-        
-        int endMoney = 5;
-        for(endMoney=5;endMoney>0 && endMoney < 100;endMoney--){
+        System.out.println("How much do you want to spend money?");
+        Scanner in = new Scanner(System.in);
+        int endMoney = 0;
+        String answer = "";
+        for(endMoney=in.nextInt();endMoney>0 && endMoney < 100;answer.equals("yes")){
             int randNum1 = random.nextInt(11);
             int randNum2 = random.nextInt(11);
             int randNum3 = random.nextInt(11);
             System.out.printf("%d %d %d \n",randNum1,randNum2,randNum3);
+            endMoney--;
             if(randNum1 == LuckyNum && randNum2 == LuckyNum && randNum3 == LuckyNum){
                 endMoney+=10;
             }else if(randNum1 != LuckyNum && randNum2 != LuckyNum && randNum3 != LuckyNum){
@@ -25,13 +28,18 @@ public class Lucky7 {
             }else if(randNum1 == LuckyNum && randNum2 != LuckyNum && randNum3 != LuckyNum){
                 endMoney+=3;
             }else
-                endMoney+=5;    
+                endMoney+=5; 
+            System.out.println("If you want to play again? Yes or No");
+            answer = in.next();
+            if(answer.equals("no")){
+                break;
+            }
         }
         if(endMoney >= 100){
-            System.out.println("You won");
+            System.out.println("You won!");
         }
         else if(endMoney == 0){
-            System.out.println("You lost");
+            System.out.println("You lost.");
         }
     }
 }
